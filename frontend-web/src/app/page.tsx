fcaa00de2f7f08653bc614f910974e903dd07d21
HenriqueSenaDev <acopsenadeveloper@gmail.com>
2025-02-04 14:18:28 -0300

    feat/front: shadcn design system setup.

--- frontend-web/src/app/page.tsx
+++ frontend-web/src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/button";
+
 export default function Home() {
   return (
-    <div className="h-screen flex items-center justify-center">
+    <div className="h-screen flex flex-col gap-5 items-center justify-center">
       <h1>Hello Developers Today!</h1>
+
+      <Button>Click me</Button>
     </div>
   );
 }
